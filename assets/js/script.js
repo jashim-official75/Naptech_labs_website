932a7dda61f837d1f66a21713c2d6a5a78a663c1
jashimuddin360 <72647659+jashimuddin360@users.noreply.github.com>
2020-10-14 01:17:15 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.element', {
+    strings: [
+        'Web Design',
+        'Web Development',
+        'Android App Development',
+        'Game Development',
+        'UX-UI Design',
+
+    ],
+    loop: true,
+    typeSpeed: 100,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+});
